b5f95cdbbc304ff291975540122b9d407baf0f61
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-02 19:14:31 +0530

    " -> 2 of 4 passed. pylint: 3.85/10 "

--- cspp1practicem3/p1/bob_counter.py
+++ cspp1practicem3/p1/bob_counter.py
@@ -2,15 +2,15 @@
 STRING = input()
 COUNT = 0
 LENGTH = int(len(STRING))
-A = 0
-B = 3
+a = 0
+b = 3
 "'Checking the string'"
 while b <= LENGTH:
-    I = STRING[A:B]
+    I = STRING[a:b]
     if I == "bob":
         COUNT = COUNT+1
-    A = A+1
-    B = B+1
+    a = a+1
+    b = b+1
 print(COUNT)
     
         
