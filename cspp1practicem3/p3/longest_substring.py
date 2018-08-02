5e68779a9e740ea16cc98099182970bc22670051
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-02 20:15:24 +0530

    " -> 5 of 5 passed. pylint: 9.23/10 "

---
+++ cspp1practicem3/p3/longest_substring.py
@@ -0,0 +1,15 @@
+"'#Enter a string'"
+S = input()
+SS = S[0]
+L = ' '
+i = 1
+for i in range(1,len(S)):
+    if SS[-1] <= S[i]:
+        SS = SS+S[i]
+    else:
+        if len(SS) > len(L):
+            L = SS
+        SS = S[i]
+    if len(SS) > len(L):
+        L = SS
+print(L)
