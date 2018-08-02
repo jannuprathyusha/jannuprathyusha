9e943a214ed274c0168745262739a25ca11fa40e
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-02 18:49:05 +0530

    " -> 0 of 4 passed. pylint: 10.00/10 "

--- cspp1practicem3/p1/vowels_counter.py
+++ cspp1practicem3/p1/vowels_counter.py
@@ -1,9 +1,9 @@
 "'#Taking the input'"
-STRING = input("Enter string:")
+STRING = input()
 COUNT = 0
 "'#counting the vowels'"
 for i in STRING:
     if i in 'aeiou':
         COUNT += 1
-print("Number of vowels are:")
+print()
 print(COUNT)
