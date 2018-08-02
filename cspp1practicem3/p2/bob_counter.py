e8ce1970b97d868a577207710dce0825beee5d25
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-02 19:27:36 +0530

    " -> 5 of 5 passed. pylint: 6.92/10 "

--- cspp1practicem3/p2/bob_counter.py
+++ cspp1practicem3/p2/bob_counter.py
@@ -7,8 +7,8 @@ B = 3
 "'Checking the string'"
 while B <= LENGTH:
     I = STRING[A:B]
-        if I == "bob":
-            COUNT = COUNT+1
+    if I == "bob":
+        COUNT = COUNT+1
     A = A+1
     B = B+1
 print(COUNT)
