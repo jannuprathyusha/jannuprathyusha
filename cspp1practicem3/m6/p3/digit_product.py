55e6f310abf0ff4b428857d3c2f67dc9fe659933
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-04 12:45:51 +0530

    " -> 4 of 7 passed. pylint: 6.25/10 "

--- cspp1practicem3/m6/p3/digit_product.py
+++ cspp1practicem3/m6/p3/digit_product.py
@@ -1,10 +1,10 @@
 N = int(input())
-PRODUCT=1
-TEMP=0
-while N!=0:
-     TEMP=N%10
-     PRODUCT=PRODUCT*TEMP
-     N=N//10
+PRODUCT = 1
+TEMP = 0
+while N != 0:
+    TEMP = N%10
+    PRODUCT = PRODUCT*TEMP
+    N = N//10
 print(PRODUCT)
     
     
