39b3b03988abb1928d70e21f45dac7ee18f76b35
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-07 18:52:01 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- cspp1practicem3/m8/p2/assignment2.py
+++ cspp1practicem3/m8/p2/assignment2.py
@@ -7,8 +7,8 @@ def sumofdigits(n_num):
     if n_num == 0:
         return 0
     return (n_num%10) + sumofdigits(n_num//10)
-'"#Output result"'
 def main():
+    '"#Output result"'
     a_num = input()
     print(sumofdigits(int(a_num)))
 if __name__ == "__main__":
