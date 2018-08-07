213d6371173364ce7860692c16826158cbc94df8
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-07 17:57:47 +0530

    " -> 10 of 10 passed. pylint: 8.00/10 "

--- cspp1practicem3/m8/p1/assignment1.py
+++ cspp1practicem3/m8/p1/assignment1.py
@@ -11,6 +11,7 @@ def factorial(n_num):
     if n_num in (0, 1):
         return 1
     return n_num*factorial(n_num-1)
+'"#Function main"'
 def main():
     a_num = input()
     print(factorial(int(a_num)))
