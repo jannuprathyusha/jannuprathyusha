af9b792f2eb18d8f13e3f21cac60e0c545952de8
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-08 19:00:47 +0530

    " -> 0 of 9 passed. pylint: 4.00/10 "

--- cspp1practicem3/m9/p2/assignment2.py
+++ cspp1practicem3/m9/p2/assignment2.py
@@ -34,7 +34,7 @@ def main():
     list1 = []
     for j in range(1, len(data)):
         list1.append(data[j][0])
-    print(get_guessed_word(secret_word, list1))
+print(get_guessed_word(secret_word, list1))
 
 if __name__ == "__main__":
     main()
