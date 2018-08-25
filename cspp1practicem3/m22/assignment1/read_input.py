5fcb9e1647a636bf15840776d82ac8b4f629fa00
jannuprathyusha <jannuprathyusha00@msitprogram.net>
2018-08-25 11:13:03 +0530

    " -> 0 of 2 passed. pylint: 0/0 "

--- cspp1practicem3/m22/assignment1/read_input.py
+++ cspp1practicem3/m22/assignment1/read_input.py
@@ -4,7 +4,7 @@ Write a python program to read multiple lines of text input and store the input 
 
 def main():
 	str=""
-    userInput = input().splitlines()
+    userInput = input().splitlines('')
 print(userInput)
 
 if __name__ == '__main__':
